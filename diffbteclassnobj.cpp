ad9a498cdd37e4a25bc8fa342d22cc0428084f49
DShreyas <daya74b@gmail.com>
2026-01-15 23:45:58 +0530

    basics

---
+++ diffbteclassnobj.cpp
@@ -0,0 +1,17 @@
+
+// CLASS V/S OBJECTS
+// Blueprint or template	Instance of a class
+// Logical entity	Physical entity
+// Declared once	Created many times
+// No memory allocation	Memory allocated when created
+// Example: Car (template)	Example: myCar, yourCar (instances)
+
+// Class Definition
+class Car {
+    String brand;
+    String model;
+}
+
+// Objects/Instances
+Car car1 = new Car();  // First object
+Car car2 = new Car();  // Second object
