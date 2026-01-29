ebb789721e98634aafd32e335b9b703b24205782
D SHREYAS <daya74b@gmail.com>
2026-01-29 22:28:43 +0530

    encapsulation

---
+++ encapsulation.cpp
@@ -0,0 +1,8 @@
+class Student {
+    private String name;
+    
+    public void setName(String name) {
+        this.name = name;  // this.name = instance variable
+                           // name = parameter
+    }
+}
