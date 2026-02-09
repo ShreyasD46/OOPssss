3fe30397fd93b53323752fdc373c2b7436357123
D SHREYAS <daya74b@gmail.com>
2026-02-09 23:57:03 +0530

    slight changes

--- copyconstruct.cpp
+++ copyconstruct.cpp
@@ -1,13 +1,6 @@
-// Copy Constructor: A copy constructor is used to declare and initialize an object from
-// another object.
-// Eg:
-// item t2(t1);
-// or
-// item t2=t1;
-// 1. The process of initializing through a copy constructor is known as copy initialization.
-// 2. t2=t1 will not invoke copy constructor. t1 and t2 are objects, assigns the values of t1 to t2.
-// 3. A copy constructor takes a reference to an object of the same class as itself as
-// an argument. 
+
+
+
 #include<iostream.h>
 class sample
 {
